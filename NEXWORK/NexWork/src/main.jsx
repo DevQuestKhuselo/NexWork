fa3bc065110dba73eefd5221c3518dc2f3276c9c
DevQuestKhuselo <sofohlo.k@gmail.com>
2026-06-27 17:59:36 +0200

    I added routers for pages

--- NEXWORK/NexWork/src/main.jsx
+++ NEXWORK/NexWork/src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import LandingPage from './pages/LandingPage.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+
+const router = createBrowserRouter([
+  {path:"/", element: <LandingPage />},
+  {path:"/register", element: <Register />},
+  {path:"/login", element: <Login />},
+  {/*path:"/clientdashboard", element: <ClientDashboard />},
+  {path:"/freelancerdashboard", element: <FreelancerDashboard />*/}
+
+  ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
